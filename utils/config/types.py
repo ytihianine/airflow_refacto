10c197e9a910ef0c52bfed9222b0f58203e4cafb
ytihianine <ytihianine.pro@gmail.com>
2025-09-16 01:01:43 +0200

    improve(dags): add dag params type

--- utils/config/types.py
+++ utils/config/types.py
@@ -1,7 +1,7 @@
 """Type definitions for configuration data structures."""
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, TypedDict, List, Dict
 
 
 @dataclass
@@ -33,3 +33,41 @@ class SelecteurConfig:
     nom_source: Optional[str] = None
     tbl_name: Optional[str] = None
     tbl_order: Optional[int] = None
+
+
+class DBParams(TypedDict):
+    prod_schema: str
+    tmp_schema: str
+
+
+class MailParams(TypedDict, total=False):
+    enable: bool
+    to: List[str]
+    cc: List[str]
+    bcc: List[str]
+
+
+class DocsParams(TypedDict):
+    lien_pipeline: str
+    lien_donnees: str
+
+
+class DagParams(TypedDict):
+    nom_projet: str
+    db: DBParams
+    mail: MailParams
+    docs: DocsParams
+
+
+# Common keys (use these constants to avoid hard-coded strings in checks)
+KEY_NOM_PROJET = "nom_projet"
+KEY_DB = "db"
+KEY_DB_PROD_SCHEMA = "prod_schema"
+KEY_DB_TMP_SCHEMA = "tmp_schema"
+KEY_MAIL = "mail"
+KEY_MAIL_ENABLE = "enable"
+KEY_MAIL_TO = "to"
+KEY_MAIL_CC = "cc"
+KEY_DOCS = "docs"
+KEY_DOCS_LIEN_PIPELINE = "lien_pipeline"
+KEY_DOCS_LIEN_DONNEES = "lien_donnees"
