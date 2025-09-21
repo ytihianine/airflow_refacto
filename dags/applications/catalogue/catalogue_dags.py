1380b4a166025b65ec4ad910b48968f93f2c7a83
ytihianine <ytihianine.pro@gmail.com>
2025-09-21 18:07:23 +0200

    refactor(dags): update dag catalogue

--- dags/applications/catalogue/catalogue_dags.py
+++ dags/applications/catalogue/catalogue_dags.py
@@ -1,21 +1,24 @@
-from airflow.decorators import dag, task_group
+from airflow.decorators import dag
 from airflow.models.baseoperator import chain
 from airflow.utils.dates import days_ago
 
 from infra.mails.sender import create_airflow_callback, MailStatus
 from utils.tasks.grist import download_grist_doc_to_s3
-from utils.tasks.sql import get_tbl_names_from_postgresql, create_tmp_tables
+from utils.tasks.sql import create_tmp_tables, copy_tmp_table_to_real_table
+from utils.tasks.s3 import copy_s3_files, del_s3_files
 
 from dags.applications.catalogue.tasks import (
-    create_task,
-    add_new_datasets,
-    sync_datasets,
+    update_referentiels,
+    source_grist,
+    source_database,
+    compare_catalogues,
+    sync_catalogues,
 )
 
 
-To = ["yanis.tihianine@finances.gouv.fr"]
-CC = ["labo-data@finances.gouv.fr"]
-
+nom_projet = "Catalogue"
+LINK_DOC_PIPELINE = ""  # noqa
+LINK_DOC_DATA = ""  # noqa
 
 default_args = {
     "owner": "airflow",
@@ -38,107 +41,46 @@ default_args = {
     description="""Pipeline qui scanne les nouvelles tables créées dans la base de données
         et synchronise la base de données et le catalogue GRIST""",
     default_args=default_args,
+    params={
+        "nom_projet": nom_projet,
+        "db": {
+            "prod_schema": "documentation",
+            "tmp_schema": "temporaire",
+        },
+        "mail": {
+            "enable": False,
+            "to": ["yanis.tihianine@finances.gouv.fr"],
+            "cc": ["labo-data@finances.gouv.fr"],
+        },
+        "docs": {
+            "lien_pipeline": LINK_DOC_PIPELINE,
+            "lien_donnees": LINK_DOC_DATA,
+        },
+    },
     on_failure_callback=create_airflow_callback(
         mail_status=MailStatus.ERROR,
     ),
     on_success_callback=create_airflow_callback(mail_status=MailStatus.SUCCESS),
-    params={
-        "nom_projet": "Catalogue des données",
-        "mail": {
-            "enable": False,
-            "To": To,
-            "CC": CC,
-        },
-        "docs": {
-            "lien_pipeline": "",
-            "lien_donnees": "",
-        },
-    },
 )
 def catalogue_dag():
-    """Config"""
-    # Variables
-    prod_schema = "documentation"
-    sqlite_file_s3_filepath = "SG/DSCI/catalogue/catalogue_sqlite.db"
-
-    """ Tasks definition """
-
-    @task_group()
-    def update_referentiels() -> None:
-        ref_frequence = create_task(
-            selecteur="ref_frequence", sqlite_file_s3_filepath=sqlite_file_s3_filepath
-        )
-        ref_contact = create_task(
-            selecteur="ref_contact", sqlite_file_s3_filepath=sqlite_file_s3_filepath
-        )
-        ref_couverture_geographique = create_task(
-            selecteur="ref_couverture_geographique",
-            sqlite_file_s3_filepath=sqlite_file_s3_filepath,
-        )
-        ref_licence = create_task(
-            selecteur="ref_licence", sqlite_file_s3_filepath=sqlite_file_s3_filepath
-        )
-        ref_service = create_task(
-            selecteur="ref_service", sqlite_file_s3_filepath=sqlite_file_s3_filepath
-        )
-        ref_source_format = create_task(
-            selecteur="ref_source_format",
-            sqlite_file_s3_filepath=sqlite_file_s3_filepath,
-        )
-        ref_structures = create_task(
-            selecteur="ref_structures", sqlite_file_s3_filepath=sqlite_file_s3_filepath
-        )
-        ref_systeme_info = create_task(
-            selecteur="ref_systeme_info",
-            sqlite_file_s3_filepath=sqlite_file_s3_filepath,
-        )
-        ref_theme = create_task(
-            selecteur="ref_theme", sqlite_file_s3_filepath=sqlite_file_s3_filepath
-        )
-        ref_type_donnees = create_task(
-            selecteur="ref_type_donnees",
-            sqlite_file_s3_filepath=sqlite_file_s3_filepath,
-        )
-
-        """ Tasks order """
-        chain(
-            [
-                ref_frequence,
-                ref_contact,
-                ref_couverture_geographique,
-                ref_licence,
-                ref_service,
-                ref_source_format,
-                ref_structures,
-                ref_systeme_info,
-                ref_theme,
-                ref_type_donnees,
-            ]
-        )
-
-    @task_group
-    def update_datasets() -> None:
-        chain(
-            add_new_datasets(tbl_name="datasets"),
-            sync_datasets(
-                selecteur="datasets",
-                prod_schema=prod_schema,
-                sqlite_file_s3_filepath=sqlite_file_s3_filepath,
-            ),
-        )
-
-    """ Task order """
+    """Task order"""
     chain(
         download_grist_doc_to_s3(
+            selecteur="grist_doc",
             workspace_id="catalogue",
             doc_id_key="grist_doc_id_catalogue",
-            s3_filepath=sqlite_file_s3_filepath,
         ),
-        get_tbl_names_from_postgresql(),
-        create_tmp_tables(
-            prod_schema=prod_schema, tbl_names_task_id="get_tbl_names_from_postgresql"
-        ),
-        [update_referentiels(), update_datasets()],
+        update_referentiels(),
+        [
+            source_grist(),
+            source_database(),
+        ],
+        compare_catalogues(),
+        sync_catalogues(),
+        create_tmp_tables(),
+        copy_tmp_table_to_real_table(),
+        copy_s3_files(bucket="dsci"),
+        del_s3_files(bucket="dsci"),
     )
 
 
--- dags/applications/catalogue/tasks.py
+++ dags/applications/catalogue/tasks.py
@@ -1,143 +1,82 @@
-from typing import Callable
-from airflow.decorators import task
+# TODO:
+# - Update referentiels
+# - Get and process catalogue from Grist
+# - Get and process catalogue from Database
+# - Compare both catalogues and log differences
+# - Save the final catalogue to s3
+# - Sync Grist and Database with the new catalogue
+
+from airflow.decorators import task_group
+from airflow.models.baseoperator import chain
+
+from utils.tasks.etl import create_grist_etl_task
 
 
-from utils.dataframe import df_info
-from utils.tasks.sql import (
-    get_conn_from_s3_sqlite,
-    get_data_from_s3_sqlite_file,
-)
-from utils.config.tasks import get_storage_rows
-
-from dags.applications.catalogue import process
-
-
-def create_task(
-    selecteur: str,
-    sqlite_file_s3_filepath: str,
-    process_func: Callable = None,
-):
-    @task(task_id=selecteur)
-    def _task(**context):
-        nom_projet = context.get("params").get("nom_projet", None)
-        if nom_projet is None:
-            raise ValueError(
-                "La variable nom_projet n'a pas été définie au niveau du DAG !"
-            )
-        # Hooks
-        s3_hook = MinioFileHandler(connection_id="minio_bucket_dsci", bucket="dsci")
-        # Get config values related to the task
-        row_selecteur = get_storage_rows(nom_projet=nom_projet, selecteur=selecteur)
-        grist_tbl_name = row_selecteur.loc[0, "nom_source"]
-
-        # Get data of table
-        conn = get_conn_from_s3_sqlite(
-            sqlite_file_s3_filepath=sqlite_file_s3_filepath, s3_hook=s3_hook
-        )
-        df = get_data_from_s3_sqlite_file(
-            grist_tbl_name=grist_tbl_name,
-            sqlite_s3_filepath=sqlite_file_s3_filepath,
-            sqlite_conn=conn,
-        )
-
-        if process_func is not None:
-            df = process.clean_and_normalize_df(df=df)
-            df_info(df=df, df_name=f"{grist_tbl_name} - Raw and normalized")
-            df = process_func(df)
-            df_info(df=df, df_name=f"{grist_tbl_name} - After processing")
-
-        # Export to file
-        s3_hook.load_bytes(
-            bytes_data=df.to_parquet(path=None, index=False),
-            key=row_selecteur.loc[0, "filepath_s3"] + ".parquet",
-            replace=True,
-        )
-
-    return _task()
-
-
-@task
-def add_new_datasets(tbl_name: str, tmp_schema: str = "temporaire") -> None:
-    from airflow.providers.postgres.hooks.postgres import PostgresHook
-
-    # Hooks
-    db_hook = PostgresHook(postgres_conn_id="db_data_store")
-
-    # Scan new datasets
-    schema_to_exclude = ", ".join(
-        ["'pg_catalog'", "'information_schema'", "'public'", "'temporaire'"]
+@task_group()
+def update_referentiels() -> None:
+    ref_frequence = create_grist_etl_task(
+        selecteur="ref_frequence",
     )
-    db_hook.run(
-        sql=f"""INSERT INTO {tmp_schema}.tmp_{tbl_name} (schema_name, table_name)
-            SELECT table_schema, table_name
-            FROM information_schema.tables ist
-            WHERE table_schema NOT IN ({schema_to_exclude})
-            AND NOT EXISTS (
-                SELECT 1
-                FROM {tmp_schema}.tmp_{tbl_name} tdd
-                WHERE tdd.schema_name = ist.table_schema
-                AND tdd.table_name = ist.table_name
-            );
-        """
+    ref_contact = create_grist_etl_task(
+        selecteur="ref_contact",
+    )
+    ref_couverture_geographique = create_grist_etl_task(
+        selecteur="ref_couverture_geographique",
+    )
+    ref_licence = create_grist_etl_task(
+        selecteur="ref_licence",
+    )
+    ref_service = create_grist_etl_task(
+        selecteur="ref_service",
+    )
+    ref_source_format = create_grist_etl_task(
+        selecteur="ref_source_format",
+    )
+    ref_structures = create_grist_etl_task(
+        selecteur="ref_structures",
+    )
+    ref_systeme_info = create_grist_etl_task(
+        selecteur="ref_systeme_info",
+    )
+    ref_theme = create_grist_etl_task(
+        selecteur="ref_theme",
+    )
+    ref_type_donnees = create_grist_etl_task(
+        selecteur="ref_type_donnees",
+    )
+
+    """ Tasks order """
+    chain(
+        [
+            ref_frequence(),
+            ref_contact(),
+            ref_couverture_geographique(),
+            ref_licence(),
+            ref_service(),
+            ref_source_format(),
+            ref_structures(),
+            ref_systeme_info(),
+            ref_theme(),
+            ref_type_donnees(),
+        ]
     )
 
 
-@task
-def sync_datasets(
-    selecteur: str, prod_schema: str, sqlite_file_s3_filepath: str, **context
-) -> None:
-    import pandas as pd
-    from airflow.providers.postgres.hooks.postgres import PostgresHook
-
-    # Hooks
-    db_hook = PostgresHook(postgres_conn_id="db_data_store")
-    s3_hook = MinioFileHandler(connection_id="minio_bucket_dsci", bucket="dsci")
-
-    # variables
-    nom_projet = context.get("params").get("nom_projet", None)
-    if nom_projet is None:
-        raise ValueError(
-            "La variable nom_projet n'a pas été définie au niveau du DAG !"
-        )
-    # Get config values related to the task
-    row_selecteur = get_storage_rows(nom_projet=nom_projet, selecteur=selecteur)
-
-    # Get data
-    df_db = db_hook.get_pandas_df(
-        sql=f"SELECT * FROM {prod_schema}.{row_selecteur.loc[0, "tbl_name"]}"
-    )
-    conn = get_conn_from_s3_sqlite(
-        sqlite_file_s3_filepath=sqlite_file_s3_filepath, s3_hook=s3_hook
-    )
-    df_grist = process.normalize_df(
-        get_data_from_s3_sqlite_file(
-            grist_tbl_name=row_selecteur.loc[0, "nom_source"],
-            sqlite_s3_filepath=sqlite_file_s3_filepath,
-            sqlite_conn=conn,
-        )
-    )
-    df_info(df=df_db, df_name="DB Datasets - Raw")
-    df_info(df=df_grist, df_name="Grist Datasets - Raw")
-
-    # Merge
-    merged = pd.merge(
-        df_db,
-        df_grist,
-        on=["schema_name", "table_name"],
-        indicator=True,
-        how="left",
-        suffixes=("_db", "_grist"),
-    )
-    df_info(df=merged, df_name="Merged dataframes - Raw")
-
-    # Export file
-
-
-@task(task_id="update_grist")
-def update_grist():
+@task_group()
+def source_grist() -> None:
     pass
 
 
-@task(task_id="update_db")
-def update_db():
+@task_group()
+def source_database() -> None:
+    pass
+
+
+@task_group()
+def compare_catalogues() -> None:
+    pass
+
+
+@task_group()
+def sync_catalogues() -> None:
     pass
