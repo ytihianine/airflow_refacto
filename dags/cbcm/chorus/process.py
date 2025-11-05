56268a09f998aacefa298e3f8a44b5de5393d362
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-11-05 09:33:40 +0000

    feat(dags): add cbcm project dag structure

---
+++ dags/cbcm/chorus/process.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+
+# ======================================================
+# Demande d'achat (DA)
+# ======================================================
+def process_demande_achat(df: pd.DataFrame) -> pd.DataFrame:
+    return df
+
+
+# ======================================================
+# Engagement juridique (EJ)
+# ======================================================
+def process_engagement_juridique(df: pd.DataFrame) -> pd.DataFrame:
+    return df
+
+
+# ======================================================
+# Demande de paiement (DP)
+# ======================================================
