5d8c1e52be2b2dff40c792cc53a4fdae11ab0c87
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-11-14 10:33:08 +0000

    fix(dags): update sql script

--- dags/dge/carto_rem/sql/tables.sql
+++ dags/dge/carto_rem/sql/tables.sql
@@ -21,7 +21,8 @@ CREATE TABLE cartographie_remuneration.ref_experience_pro (
 -- table: cartographie_remuneration.ref_base_recrutement
 CREATE TABLE cartographie_remuneration.ref_base_remuneration (
 	id SERIAL primary key,
-	base_remuneration TEXT
+	base_remuneration TEXT,
+  base_rem_dge TEXT
 );
 
 -- table: cartographie_remuneration.ref_base_revalorisation
@@ -95,7 +96,7 @@ CREATE TABLE cartographie_remuneration.agent_poste (
     matricule_agent BIGINT REFERENCES cartographie_remuneration.agent(matricule_agent),
     numero_poste TEXT,
     categorie TEXT,
-    fonction_anais TEXT,
+    libelle_du_poste TEXT,
     fonction_dge_libelle_court TEXT,
     fonction_dge_libelle_long TEXT,
     date_recrutement_structure DATE
@@ -107,15 +108,27 @@ CREATE TABLE cartographie_remuneration.agent_remuneration (
     -- base_recrutement TEXT, = Niveau de diplôme ?
     indice_majore INT,
     type_indemnitaire TEXT,
+    id_base_remuneration INT,
     region_indemnitaire TEXT,
     region_indemnitaire_valeur FLOAT,
+    remuneration_principale FLOAT,
     total_indemnitaire_annuel FLOAT,
     total_annuel_ifse FLOAT,
     totale_brute_annuel FLOAT,
     plafond_part_variable FLOAT,
     plafond_part_variable_collective FLOAT,
     present_cartographie BOOLEAN,
-    observations TEXT
+    observations TEXT,
+    FOREIGN KEY(id_base_remuneration) REFERENCES cartographie_remuneration.ref_base_remuneration(id)
+);
+
+CREATE TABLE cartographie_remuneration.agent_experience_pro (
+  id SERIAL,
+  matricule_agent BIGINT,
+  experience_pro_totale DOUBLE PRECISION,
+  experience_pro_qualifiante DOUBLE PRECISION,
+  PRIMARY KEY (id),
+  FOREIGN KEY(matricule_agent) REFERENCES cartographie_remuneration.agent(matricule_agent)
 );
 
 /*
@@ -124,7 +137,11 @@ CREATE TABLE cartographie_remuneration.agent_remuneration (
 CREATE MATERIALIZED VIEW cartographie_remuneration.vw_agent_cartographie_remuneration AS
 WITH cte_niveau_diplome AS (
   SELECT
-    crad.matricule_agent as matricule_agent, crad.id_niveau_diplome as id_niveau_diplome, crrnd.niveau_diplome
+    crad.matricule_agent as matricule_agent,
+    crad.id_niveau_diplome as id_niveau_diplome,
+    crrnd.niveau_diplome as niveau_diplome,
+    crad.libelle_diplome as libelle_diplome,
+    crad.annee_d_obtention as annee_d_obtention
   FROM cartographie_remuneration.ref_niveau_diplome crrnd
   LEFT JOIN cartographie_remuneration.agent_diplome crad
     ON crrnd.id = crad.id_niveau_diplome
@@ -135,18 +152,25 @@ SELECT
   cra.nom_usuel,
   cra.prenom,
   cra.date_de_naissance,
+  date_part('year', cra.date_de_naissance)::int as annee_naissance,
   cra.genre,
+  cra.qualite_statutaire,
+  cra.date_acces_corps,
+  date_part('year', cra.date_acces_corps)::int as annee_acces_corps,
   cra.corps,
   cra.grade,
   cra.echelon,
-  -- agent_diplome
-  crad.id_niveau_diplome,
-  crad.libelle_diplome,
-  crad.annee_d_obtention,
+  -- agent_contrat
+  crac.date_debut_contrat_actuel_dge,
+  crac.date_entree_dge,
   -- agent_experience_pro
+  craep.experience_pro_totale,
+  craep.experience_pro_qualifiante_sur_poste,
   -- agent_poste
   crap.categorie,
-  crap.type_de_fonction_libelle_court,
+  crap.libelle_du_poste,
+  crap.fonction_dge_libelle_court,
+  crap.fonction_dge_libelle_long,
   crap.date_recrutement_structure,
   -- agent_remuneration
   crar.indice_majore,
@@ -158,15 +182,27 @@ SELECT
   crar.totale_brute_annuel,
   crar.plafond_part_variable,
   crar.plafond_part_variable_collective,
+  CASE
+    WHEN crar.plafond_part_variable IS NULL AND crar.plafond_part_variable_collective IS NULL
+      THEN NULL
+    ELSE
+      COALESCE(crar.plafond_part_variable, 0) + COALESCE(crar.plafond_part_variable_collective, 0)
+  END as plafond_part_variable_totale,
   crar.present_cartographie,
   -- cte_diplome
-  cte_nd.niveau_diplome
+  cte_diplome.niveau_diplome,
+  cte_diplome.id_niveau_diplome,
+  cte_diplome.libelle_diplome,
+  cte_diplome.annee_d_obtention
 FROM cartographie_remuneration.agent cra
-LEFT JOIN cartographie_remuneration.agent_diplome crad
-  ON cra.matricule_agent = crad.matricule_agent
+LEFT JOIN cartographie_remuneration.agent_contrat crac
+  ON cra.matricule_agent = crac.matricule_agent
+LEFT JOIN cartographie_remuneration.agent_experience_pro craep
+  ON cra.matricule_agent = craep.matricule_agent
 LEFT JOIN cartographie_remuneration.agent_poste crap
   ON cra.matricule_agent = crap.matricule_agent
 LEFT JOIN cartographie_remuneration.agent_remuneration crar
   ON cra.matricule_agent = crar.matricule_agent
-LEFT JOIN cte_niveau_diplome cte_nd
-  ON cra.matricule_agent = cte_nd.matricule_agent
+LEFT JOIN cte_niveau_diplome cte_diplome
+  ON cra.matricule_agent = cte_diplome.matricule_agent
+;
