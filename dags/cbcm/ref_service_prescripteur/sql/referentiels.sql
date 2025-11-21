8717dd94ce79387d4346c0ae239d8e6d8190ac5b
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-11-21 22:37:12 +0000

    fix(dags): update prod schema value

--- dags/cbcm/ref_service_prescripteur/sql/referentiels.sql
+++ dags/cbcm/ref_service_prescripteur/sql/referentiels.sql
@@ -1,28 +1,28 @@
-CREATE SCHEMA donnees_comptable;
+CREATE SCHEMA donnee_comptable;
 
-CREATE TABLE donnees_comptable."ref_service_prescripteur_pilotage" (
+CREATE TABLE donnee_comptable."ref_service_prescripteur_pilotage" (
   "id" integer PRIMARY KEY,
   "service_prescripteur" text
 );
 
-CREATE TABLE donnees_comptable."ref_service_depense" (
+CREATE TABLE donnee_comptable."ref_service_depense" (
   "id" integer PRIMARY KEY,
   "service_depense" text
 );
 
-CREATE TABLE donnees_comptable."ref_prog" (
+CREATE TABLE donnee_comptable."ref_prog" (
   "id" integer PRIMARY KEY,
   "prog" text
 );
 
-CREATE TABLE donnees_comptable."ref_bop" (
+CREATE TABLE donnee_comptable."ref_bop" (
   "id" integer PRIMARY KEY,
   "prog" int,
   "bop" text,
   FOREIGN KEY ("prog") REFERENCES "ref_prog" ("id")
 );
 
-CREATE TABLE donnees_comptable."ref_cc" (
+CREATE TABLE donnee_comptable."ref_cc" (
   "id" integer PRIMARY KEY,
   "prog" int,
   "bop" int,
@@ -33,7 +33,7 @@ CREATE TABLE donnees_comptable."ref_cc" (
   FOREIGN KEY ("uo") REFERENCES "ref_uo" ("id")
 );
 
-CREATE TABLE donnees_comptable."ref_uo" (
+CREATE TABLE donnee_comptable."ref_uo" (
   "id" integer PRIMARY KEY,
   "prog" int,
   "bop" int,
@@ -42,7 +42,7 @@ CREATE TABLE donnees_comptable."ref_uo" (
   FOREIGN KEY ("bop") REFERENCES "ref_bop" ("id")
 );
 
-CREATE TABLE donnees_comptable."service_prescripteur" (
+CREATE TABLE donnee_comptable."service_prescripteur" (
   "id" integer PRIMARY KEY,
   "centre_financer" text,
   "centre_de_cout" text,
@@ -64,7 +64,7 @@ CREATE TABLE donnees_comptable."service_prescripteur" (
   FOREIGN KEY ("designation_cc") REFERENCES "ref_cc" ("id")
 );
 
-CREATE TABLE donnees_comptable."ref_service_prescripteur_choisi" (
+CREATE TABLE donnee_comptable."ref_service_prescripteur_choisi" (
   "id" integer PRIMARY KEY,
   "service_prescripteur" text
 );
