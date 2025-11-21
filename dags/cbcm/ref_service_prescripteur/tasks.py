d1e1f4882876fdb7c814255bf14ed1ed11720c55
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-11-21 17:50:11 +0000

    fix(dags): add normalization function to every grist task in cbcm referentiel dag

--- dags/cbcm/ref_service_prescripteur/tasks.py
+++ dags/cbcm/ref_service_prescripteur/tasks.py
@@ -6,7 +6,7 @@ from utils.config.types import ALL_PARAM_PATHS
 from utils.tasks.etl import create_grist_etl_task
 
 from dags.cbcm.ref_service_prescripteur import process
-
+from utils.control.structures import normalize_grist_dataframe
 
 validate_params = create_validate_params_task(
     required_paths=ALL_PARAM_PATHS,
@@ -20,34 +20,42 @@ def grist_source() -> None:
     ref_prog = create_grist_etl_task(
         selecteur="ref_prog",
         process_func=process.process_ref_prog,
+        normalisation_process_func=normalize_grist_dataframe,
     )
     ref_bop = create_grist_etl_task(
         selecteur="ref_bop",
         process_func=process.process_ref_bop,
+        normalisation_process_func=normalize_grist_dataframe,
     )
     ref_uo = create_grist_etl_task(
         selecteur="ref_uo",
         process_func=process.process_ref_uo,
+        normalisation_process_func=normalize_grist_dataframe,
     )
     ref_cc = create_grist_etl_task(
         selecteur="ref_cc",
         process_func=process.process_ref_cc,
+        normalisation_process_func=normalize_grist_dataframe,
     )
     ref_sdep = create_grist_etl_task(
         selecteur="ref_sdep",
         process_func=process.process_ref_sdep,
+        normalisation_process_func=normalize_grist_dataframe,
     )
     ref_sp_choisi = create_grist_etl_task(
         selecteur="ref_sp_choisi",
         process_func=process.process_ref_sp_choisi,
+        normalisation_process_func=normalize_grist_dataframe,
     )
     ref_sp_pilotage = create_grist_etl_task(
         selecteur="ref_sp_pilotage",
         process_func=process.process_ref_sp_pilotage,
+        normalisation_process_func=normalize_grist_dataframe,
     )
     sp = create_grist_etl_task(
         selecteur="sp",
         process_func=process.process_service_prescripteur,
+        normalisation_process_func=normalize_grist_dataframe,
     )
 
     chain(
