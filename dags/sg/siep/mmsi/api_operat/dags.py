c596e6de308f8a47f689bb0745bf83874c24910d
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-08-05 14:35:34 +0000

    fix(dags/api_operat): use storage_options instead of selecteur_config

--- dags/sg/siep/mmsi/api_operat/dags.py
+++ dags/sg/siep/mmsi/api_operat/dags.py
@@ -59,7 +59,7 @@ def api_operat_ademe() -> None:
         create_tmp_tables(reset_id_seq=False),
         import_file_to_db.expand(selecteur_config=selecteur_configs),
         ensure_partition.expand(selecteur_config=selecteur_configs),
-        copy_tmp_table_to_real_table(selecteur_config=selecteur_configs),
+        copy_tmp_table_to_real_table(storage_options=storage_options),
         copy_s3_files(),
         del_s3_files(),
     )
