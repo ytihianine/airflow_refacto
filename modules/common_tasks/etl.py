5ae3a65c82e7722b29462741190476c30eda95ad
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-08-12 10:16:42 +0000

    fix(common_task/etl): convert snapshot_id_parent only if is it not None

--- modules/common_tasks/etl.py
+++ modules/common_tasks/etl.py
@@ -25,10 +25,9 @@ def _add_metadata(df: pd.DataFrame, nom_projet: str) -> pd.DataFrame:
 
     df["import_timestamp"] = metadata["import_timestamp"]
     df["snapshot_id"] = str(metadata["snapshot_id"])
+    df["snapshot_id_parent"] = metadata["snapshot_id_parent"]
 
-    if metadata["snapshot_id_parent"] is None:
-        df["snapshot_id_parent"] = str(metadata["snapshot_id_parent"])
-    else:
+    if metadata["snapshot_id_parent"] is not None:
         df["snapshot_id_parent"] = str(metadata["snapshot_id_parent"])
 
     return df
