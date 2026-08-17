186eaa672a32129d9237e629531b08d8d4941792
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-08-17 15:09:41 +0000

    fix(dags/siep): update selecteur_options of dag fcu and georisques

--- dags/sg/siep/mmsi/eligibilite_fcu/config.py
+++ dags/sg/siep/mmsi/eligibilite_fcu/config.py
@@ -1,3 +1,12 @@
+from modules.enums.database import LoadStrategy
+from modules.types.projet import SelecteurStorageOptions
+
 dag_id_fcu = "eligibilite_fcu"
 nom_projet_fcu = "France Chaleur Urbaine (FCU)"
-storage_options = {}
+
+storage_options = {
+    "fcu": SelecteurStorageOptions(
+        write_to_db=False,
+    ),
+    "fcu_result": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND),
+}
--- dags/sg/siep/mmsi/georisques/config.py
+++ dags/sg/siep/mmsi/georisques/config.py
@@ -1,4 +1,12 @@
+from modules.enums.database import LoadStrategy
+from modules.types.projet import SelecteurStorageOptions
+
 nom_projet_georisque = "Géorisques"
 dag_id_georisque = "georisques_batiments"
 
-storage_options = {}
+storage_options = {
+    "bien_db": SelecteurStorageOptions(
+        write_to_db=False,
+    ),
+    "georisques": SelecteurStorageOptions(load_strategy=LoadStrategy.APPEND),
+}
